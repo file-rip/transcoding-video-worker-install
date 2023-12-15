19809c62fdfcd400a1530735c90318f33a88f3a4
file-rip <147386178+file-rip@users.noreply.github.com>
2023-12-15 15:26:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-# Automatically compile and install FFMPEG with NVIDIA hardware acceleration in nvidia/cuda:12.0-devel-ubuntu22.04
-# Includes cuvid, cuda, nvenc, nvdec, and libnpp
+
+# Automatically compile and install FFMPEG with NVIDIA hardware acceleration on Debian 12
+# Includes cuvid, cuda, nvenc, nvdec, and non-free libnpp
 # Based on:
 #  https://www.tal.org/tutorials/ffmpeg_nvidia_encode
 #  https://developer.nvidia.com/blog/nvidia-ffmpeg-transcoding-guide/
@@ -10,41 +10,43 @@ set -e
 
 suite=$(. /etc/os-release && echo $VERSION_CODENAME)*
 
-# Update package list
-apt-get update
+echo "deb-src http://deb.debian.org/debian/ bookworm main contrib non-free" | tee -a /etc/apt/sources.list
 
-# Install necessary tools and dependencies
-apt-get install -y wget git build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+apt update
 
-# Clone and install nv-codec-headers
+# Install libavcodec-extra manually so the build-deps step doesn't pull the problematic libavcodec59
+# libjs-bootstrap is a dependency of ffmpeg-doc
+# devscripts contains the dch command
+sudo apt-get install -y libavcodec-extra libjs-bootstrap devscripts
+sudo apt-mark auto libavcodec-extra libjs-bootstrap devscripts
+
+sudo apt-get build-dep ffmpeg -t $suite
+
 mkdir -p ffmpeg-deb/src
 cd ffmpeg-deb
-if [[ -d nv-codec-headers ]]; then
-    cd nv-codec-headers
-    git fetch --tags
+if [[ -d nv-codec-headers ]]
+then
+  cd nv-codec-headers
+  git fetch --tags
 else
-    git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-    cd nv-codec-headers
+  git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+  cd nv-codec-headers
 fi
-
-# Checkout the latest release
+# Checkout latest release, intead of HEAD. The Debian driver in stable may not yet support the pre-release API.
 git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
-make PREFIX=/usr/local/cuda
-make install
+make
+sudo make install
 cd ../src
 rm -rf ./*
+apt-get source ffmpeg -t $suite
 
-#install ffmpeg
-
-git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
-cd ffmpeg
-
-# Checkout the latest release
-git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
-
-./configure  --enable-cuda-nvcc --enable-cuda --enable-cuvid --enable-nvdec --enable-nvenc --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --enable-libnpp --enable-nonfree  --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
-
+cd ffmpeg-*
+sed -i 's/--enable-sdl2/--enable-sdl2 --enable-cuda --enable-cuvid --enable-nvdec --enable-nvenc --enable-libnpp --enable-nonfree/' debian/rules
+DEBEMAIL="root@local" DEBFULLNAME="script" dch --local "+nvidiasupport" "Compiled with support for nvidia hardware acceleration"
+DEB_BUILD_OPTIONS="nocheck notest" dpkg-buildpackage -r -nc --jobs=auto --no-sign
 cd ..
 
+# Install all built packages, except the non-extra variants of libavfilter, libavcodec and libavformat
+sudo dpkg -i $(ls *.deb | grep -Ev "(libavfilter|libavcodec|libavformat)[0-9]+_")
 echo "Verification:"
 ffmpeg -codecs 2> /dev/null | grep nvenc
