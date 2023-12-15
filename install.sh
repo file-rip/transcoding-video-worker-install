72811816c2f39c577d31eebab93c5183383db3fc
file-rip <147386178+file-rip@users.noreply.github.com>
2023-12-15 08:40:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,108 +1,48 @@
 #!/bin/bash
-set -e # Exit on any error
+# Automatically compile and install FFMPEG with NVIDIA hardware acceleration on Debian 12
+# Includes cuvid, cuda, nvenc, nvdec, and non-free libnpp
+# Based on:
+#  https://www.tal.org/tutorials/ffmpeg_nvidia_encode
+#  https://developer.nvidia.com/blog/nvidia-ffmpeg-transcoding-guide/
 
-USR_LOCAL_PREFIX="/usr/local"
-CUDA_HOME=$USR_LOCAL_PREFIX/cuda
-HOME_DIR=$HOME
-SRC_DIR=$HOME_DIR/sources
+# Abort on error
+set -e
 
-CPUS=$(nproc)
-LOG_FILE="$HOME_DIR/install.log"
-LOCAL_TMP="$HOME_DIR/sources/local-tmp"
-mkdir -p $LOCAL_TMP
+suite=$(source /etc/os-release && echo $VERSION_CODENAME)*
 
-# Create source directory
-mkdir -p $SRC_DIR
-cd $SRC_DIR
+# Install libavcodec-extra manually so the build-deps step doesn't pull the problematic libavcodec59
+# libjs-bootstrap is a dependency of ffmpeg-doc
+# devscripts contains the dch command
+sudo apt-get install libavcodec-extra libjs-bootstrap devscripts
+sudo apt-mark auto libavcodec-extra libjs-bootstrap devscripts
 
-# Install system utilities and updates
-install_utils() {
-    if [ -n "$(command -v dnf)" ]; then
-        package_manager="dnf"
-    elif [ -n "$(command -v apt)" ]; then
-        package_manager="apt"
-    else
-        echo "Neither DNF nor APT package manager found. Exiting."
-        exit 1
-    fi
+sudo apt-get build-dep ffmpeg -t $suite
+sudo apt-get install nvidia-cuda-toolkit -t $suite
+mkdir -p ffmpeg-deb/src
+cd ffmpeg-deb
+if [[ -d nv-codec-headers ]]
+then
+  cd nv-codec-headers
+  git fetch --tags
+else
+  git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+  cd nv-codec-headers
+fi
+# Checkout latest release, intead of HEAD. The Debian driver in stable may not yet support the pre-release API.
+git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
+make
+sudo make install
+cd ../src
+rm -rf ./*
+apt-get source ffmpeg -t $suite
 
-    echo "Updating packages..."
-    $package_manager -y update
+cd ffmpeg-*
+sed -i 's/--enable-sdl2/--enable-sdl2 --enable-cuda --enable-cuvid --enable-nvdec --enable-nvenc --enable-libnpp --enable-nonfree/' debian/rules
+DEBEMAIL="root@local" DEBFULLNAME="script" dch --local "+nvidiasupport" "Compiled with support for nvidia hardware acceleration"
+DEB_BUILD_OPTIONS="nocheck notest" dpkg-buildpackage -r -nc --jobs=auto --no-sign
+cd ..
 
-    echo "Installing packages..."
-    if [ "$package_manager" = "dnf" ]; then
-        $package_manager -y groupinstall "Development Tools"
-        $package_manager install -y git autoconf openssl-devel cmake3 htop iotop yasm nasm jq freetype-devel fribidi-devel harfbuzz-devel fontconfig-devel bzip2-devel
-    elif [ "$package_manager" = "apt" ]; then
-        export DEBIAN_FRONTEND=noninteractive;
-        export NEEDRESTART_MODE=a;
-        $package_manager install -y build-essential git autoconf libtool libssl-dev cmake htop iotop yasm nasm jq libfreetype6-dev libfribidi-dev libharfbuzz-dev libfontconfig1-dev libbz2-dev
-    fi
-
-    echo "Success: Updates and packages installed."
-}
-
-# Helper functionS to check installation status
-check_installation() {
-    if [ -f "$1" ]; then
-        echo "Success : $2 Installed"
-    else
-        echo "Error : $2 Installation Failed"
-        echo "Exiting script due to installation failure."
-        exit 1
-    fi
-}
-
-install_ffmpeg_prereqs() {
-
-    # Install ffnvcodec FFmpeg with NVIDIA GPU accelsetup_gpueration
-    git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git && cd nv-codec-headers && make install PREFIX="$USR_LOCAL_PREFIX"
-    check_installation "$USR_LOCAL_PREFIX/include/ffnvcodec/nvEncodeAPI.h" "Nvidia ffnvcodec"
-
-    # Install X264 (H.264 Codec)
-    git clone --depth 1 https://code.videolan.org/videolan/x264.git &&
-        cd x264 &&
-        PKG_CONFIG_PATH="$USR_LOCAL_PREFIX/lib/pkgconfig" ./configure \
-            --enable-shared --disable-static \
-            --prefix="$USR_LOCAL_PREFIX" \
-            --bindir="/usr/bin" &&
-        make -j $CPUS &&
-        make install
-    cd ..
-    check_installation "$USR_LOCAL_PREFIX/lib/libx264.so" "X264"
-}
-
-install_ffmpeg() {
-    NVIDIA_CFLAGS="-I$CUDA_HOME/include"
-    NVIDIA_LDFLAGS="-L$CUDA_HOME/lib64"
-    NVIDIA_FFMPEG_OPTS="--enable-cuda-nvcc --nvcc=$CUDA_HOME/bin/nvcc --enable-cuda --enable-cuvid --enable-nvenc"
-
-    # Install FFMPEG (AV1 Codec Library)
-    git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/ &&
-        cd ffmpeg &&
-        PKG_CONFIG_PATH="$USR_LOCAL_PREFIX/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig:$USR_LOCAL_PREFIX/lib/pkgconfig" \
-            ./configure \
-            --prefix="$USR_LOCAL_PREFIX" \
-            --disable-static --enable-shared \
-            --extra-cflags="-I$USR_LOCAL_PREFIX/include $NVIDIA_CFLAGS" \
-            --extra-ldflags="-L$USR_LOCAL_PREFIX/lib $NVIDIA_LDFLAGS" \
-            --extra-libs='-lpthread -lm' \
-            --bindir="$USR_LOCAL_PREFIX/bin" \
-            --enable-gpl \
-            --enable-libx264 \
-            --enable-nonfree \
-            --enable-openssl \
-            $NVIDIA_FFMPEG_OPTS &&
-        make -j $CPUS &&
-        make install
-    cd ..
-    check_installation "$USR_LOCAL_PREFIX/bin/ffmpeg" "ffmpeg"
-    check_installation "$USR_LOCAL_PREFIX/bin/ffprobe" "ffprobe"
-    ldconfig
-}
-
-# Execute Functions
-install_utils
-install_ffmpeg_prereqs
-install_ffmpeg
-rm -fr $SRC_DIR
+# Install all built packages, except the non-extra variants of libavfilter, libavcodec and libavformat
+sudo dpkg -i $(ls *.deb | grep -Ev "(libavfilter|libavcodec|libavformat)[0-9]+_")
+echo "Verification:"
+ffmpeg -codecs 2> /dev/null | grep nvenc
