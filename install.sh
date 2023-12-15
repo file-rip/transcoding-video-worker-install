49d2e9a707bbe8c27ea198f3e2d4e7eea0806d84
file-rip <147386178+file-rip@users.noreply.github.com>
2023-12-15 09:45:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Automatically compile and install FFMPEG with NVIDIA hardware acceleration on Debian 12
+# Automatically compile and install FFMPEG with NVIDIA hardware acceleration on Debian 12 Bookworm Slim
 # Includes cuvid, cuda, nvenc, nvdec, and non-free libnpp
 # Based on:
 #  https://www.tal.org/tutorials/ffmpeg_nvidia_encode
@@ -10,15 +10,32 @@ set -e
 
 suite=$(. /etc/os-release && echo $VERSION_CODENAME)*
 
+# Update package list
+sudo apt-get update
+
+# Install necessary tools and dependencies
+sudo apt-get install -y wget git make
+
 # Install libavcodec-extra manually so the build-deps step doesn't pull the problematic libavcodec59
 # libjs-bootstrap is a dependency of ffmpeg-doc
 # devscripts contains the dch command
-apt-get update
-apt-get -y install libavcodec-extra libjs-bootstrap devscripts
-apt-mark auto libavcodec-extra libjs-bootstrap devscripts
+sudo apt-get install -y libavcodec-extra libjs-bootstrap devscripts
+sudo apt-mark auto libavcodec-extra libjs-bootstrap devscripts
 
-apt-get build-dep ffmpeg -t $suite
-apt-get -y install nvidia-cuda-toolkit -t $suite
+# Add non-free repositories if not already present
+echo "deb http://deb.debian.org/debian/ $suite main contrib non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ $suite main contrib non-free" | sudo tee -a /etc/apt/sources.list
+
+# Update package list after adding non-free repositories
+sudo apt-get update
+
+# Install build dependencies for ffmpeg
+sudo apt-get build-dep -y ffmpeg -t $suite
+
+# Install NVIDIA CUDA Toolkit
+sudo apt-get install -y nvidia-cuda-toolkit -t $suite
+
+# Clone and install nv-codec-headers
 mkdir -p ffmpeg-deb/src
 cd ffmpeg-deb
 if [[ -d nv-codec-headers ]]
@@ -29,14 +46,16 @@ else
   git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
   cd nv-codec-headers
 fi
-# Checkout latest release, intead of HEAD. The Debian driver in stable may not yet support the pre-release API.
+
+# Checkout latest release, instead of HEAD. The Debian driver in stable may not yet support the pre-release API.
 git checkout $(git describe --tags $(git rev-list --tags --max-count=1))
 make
-make install
+sudo make install
 cd ../src
 rm -rf ./*
 apt-get source ffmpeg -t $suite
 
+# Modify ffmpeg package for NVIDIA hardware acceleration
 cd ffmpeg-*
 sed -i 's/--enable-sdl2/--enable-sdl2 --enable-cuda --enable-cuvid --enable-nvdec --enable-nvenc --enable-libnpp --enable-nonfree/' debian/rules
 DEBEMAIL="root@local" DEBFULLNAME="script" dch --local "+nvidiasupport" "Compiled with support for nvidia hardware acceleration"
@@ -44,6 +63,6 @@ DEB_BUILD_OPTIONS="nocheck notest" dpkg-buildpackage -r -nc --jobs=auto --no-sig
 cd ..
 
 # Install all built packages, except the non-extra variants of libavfilter, libavcodec and libavformat
-dpkg -i $(ls *.deb | grep -Ev "(libavfilter|libavcodec|libavformat)[0-9]+_")
+sudo dpkg -i $(ls *.deb | grep -Ev "(libavfilter|libavcodec|libavformat)[0-9]+_")
 echo "Verification:"
 ffmpeg -codecs 2> /dev/null | grep nvenc
