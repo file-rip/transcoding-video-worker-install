61458e041a1ed2096711bc5f6d189da7c1d0b660
file-rip <147386178+file-rip@users.noreply.github.com>
2023-12-17 16:26:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ rm -rf nv-codec-headers
 
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 cd ffmpeg
-./configure --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --nvccflags="-gencode arch=compute_52,code=sm_52 -O2" --enable-gpl --enable-libx264 --enable-libx265
+./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared --enable-libx264 --enable-libx265
 make -j$(nproc)
 make install
 
