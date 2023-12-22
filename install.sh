9fd7b8b8ee5e04d1f92e038f02ce91db82c7c77d
file-rip <147386178+file-rip@users.noreply.github.com>
2023-12-22 13:07:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,56 @@
 # Abort on error
 set -e
 
+NV_CODEC_HEADERS_VERSION=12.0.16.1
+FFMPEG_VERSION=5.1.4
+
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 cd nv-codec-headers
-make
+git checkout n${NV_CODEC_HEADERS_VERSION}
 make install
 cd ..
 
 rm -rf nv-codec-headers
 
-git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
-cd ffmpeg
-./configure --enable-nonfree --enable-cuda-nvcc --enable-gpl --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared --enable-libx264 --enable-libx265
-make -j$(nproc)
+wget https://github.com/FFmpeg/FFmpeg/archive/refs/tags/n${FFMPEG_VERSION}.tar.gz
+tar -xf n${FFMPEG_VERSION}.tar.gz
+cd FFmpeg-n${FFMPEG_VERSION}
+
+prefix=/usr/
+ccap=75
+
+./configure \
+  --prefix="${prefix}" \
+  --extra-cflags='-I/usr/local/cuda/include' \
+  --extra-ldflags='-L/usr/local/cuda/lib64' \
+  --nvccflags="-gencode arch=compute_${ccap},code=sm_${ccap} -O2" \
+  --disable-doc \
+  --enable-decoder=aac \
+  --enable-decoder=h264 \
+  --enable-decoder=h264_cuvid \
+  --enable-decoder=rawvideo \
+  --enable-indev=lavfi \
+  --enable-encoder=libx264 \
+  --enable-encoder=h264_nvenc \
+  --enable-demuxer=mov \
+  --enable-muxer=mp4 \
+  --enable-filter=scale \
+  --enable-filter=testsrc2 \
+  --enable-protocol=file \
+  --enable-protocol=https \
+  --enable-gnutls \
+  --enable-shared \
+  --enable-gpl \
+  --enable-nonfree \
+  --enable-cuda-nvcc \
+  --enable-libx264 \
+  --enable-nvenc \
+  --enable-cuvid \
+  --enable-nvdec
+
+make clean
+make -j
 make install
+
 
 rm -rf ffmpeg
